c73ce9e8ef5bcebf9bee5048b78caeee6727a6c6
Adeline <adeline80916@gmail.com>
2015-01-22 21:50:42 +0800

    from length() to __len__()

--- cha3.py
+++ cha3.py
@@ -1,6 +1,21 @@
 with open("cha3.txt") as f:
 	asciis = f.read().splitlines()[0].split(' ')[2]
-length = len(str(asciis))
-asciis = [asciis[i:i+8] for i in range(0, length, 8)]
+asciis = [asciis[i:i+8] for i in range(0, asciis.__len__(), 8)]
 asciis = [int(ele, 2) for ele in asciis]
 print "".join(map(chr, asciis))
+
+
+# __len__() is different from sys.getsizeof()
+
+'''
+Two ways to split file into groups per 8-bit:
+
+1.
+asciis = f.read().splitlines()[0].split(' ')[2]
+
+2.
+lines = []
+for i in range(0, asciis.__len__(), 8):
+	lines.append(asciis[i:i+8])
+print lines
+'''
