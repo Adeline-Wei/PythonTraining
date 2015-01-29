2bbdf01d836f1b0de72c94fee7ee6816cc38045f
Adeline <adeline80916@gmail.com>
2015-01-29 23:16:00 +0800

    Modify comments

--- crypt_xor.py
+++ crypt_xor.py
@@ -9,5 +9,18 @@ with open("encrypt_xor.jpg", "rb") as f:
 	message = f.read()
 
 key = "doraemon"
-with open("doraemon.jpg", 'w') as p:
+with open("doraemon.jpg", 'wb') as p:
 	p.write(''.join(chr(ord(c)^ord(k)) for c, k in izip(message, cycle(key))))
+
+
+'''
+x = [1, 2, 3, 4, 5, 6]
+y = [a, b, c, d]
+
+--- izip ---
+>>> (1,a) (2,b) (3,c) (4,d)
+
+--- cycle ---
+>>> (1,a) (2,b) (3,c) (4,d) (5,a) (6,b)
+
+'''
